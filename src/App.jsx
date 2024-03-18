6f333bded325e3c9e08b62bea345ddc77e230077
M.SHYJAL <shyjalsachu786@gmail.com>
2024-03-18 12:05:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+import './App.css'
+import CounterMain from './components/CounterMain'
+
+function App() {
+  return (
+    <>
+
+        <CounterMain/>
+
+    </>
+  )
+}
+
+export default App
